ea25a75c52f433886e8ac40687f34c16cd9ff0a5
Son Pham <son.pham@aia.com>
2025-07-15 21:37:15 +0700

    update swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 const authRoutes = require('./routes/auth.routes');
 const questionRoutes = require('./routes/question.routes');
@@ -14,10 +15,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use('/api', authRoutes);
 app.use('/api/questions', questionRoutes);
 app.use('/api/exams', examRoutes);
 app.use('/api/reports', reportRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
